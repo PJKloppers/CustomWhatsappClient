633935ccc92642da3f42a59eccf50fca4a13981e
Paul Kloppers <paul.kloppers@outlook.com>
2022-08-10 03:28:15 +0200

    added open file Dialog

--- frontend/script.js
+++ frontend/script.js
@@ -1,36 +1,29 @@
+const {ipcRenderer} = require('electron');
+
+
+
+function opendialog(){
+    sendData('open-dialog', 'open');
+}
+    
+
+
+function sendData(topic, data) {
+    ipcRenderer.send(topic, data);
+}
+
+ipcRenderer.on('respond_data', (event, data) => {
+    alert("Data received " + data);
+} );
+
+// ipcrenderer on open-dialog responce
+ipcRenderer.on('open-dialog', (event, file) => {
+    alert("file received " + file);
+} );
+
 
     
-    const {ipcRenderer} = require('electron');
-  
-    const qrcode = require('qrcode');
-
-            
-
-    function sendData(topic, data) {
-       
-        ipcRenderer.send(topic, data);
-       
-    }
-
-    ipcRenderer.on('respond_data', (event, data) => {
-        alert("Data received " + data);
-    } );
-
-    //IPC receive qr data
-    ipcRenderer.on('qr', (event, data) => {
-        
-        qrcode.toDataURL(data, function (err, url) {
-            const img = document.createElement('img');
-            img.src = url;
-            img.className = 'qr';
-            img.width=200;
-            document.getElementById('qrcode').innerHTML = '';
-            document.getElementById('qrcode').appendChild(img);
-        } );
-
-    }
-    );
-
+    
     
 
     
--- src/main.js
+++ src/main.js
@@ -1,24 +1,94 @@
-const {app, BrowserWindow, Menu, ipcMain} = require('electron');
+const {app, BrowserWindow, Menu, ipcMain,dialog} = require('electron');
 const path = require('path');
 const url = require('url');
+const fs = require("fs");
+
+let mainWindow;
+
 
 const { Client, Location, List, Buttons, LocalAuth} = require('whatsapp-web.js');
 
 const wclient = new Client(
    { puppeteer: 
-    { headless: false ,
+    { 
+        headless: false,
         args: ['--disable-background-mode','--no-sandbox','--no-experiments','--app=https://web.whatsapp.com/','--window-size=900,740']
-    }
-}
-);
+    },
+   // authStrategy: new LocalAuth(),
+});
 
-let mainWindow;
+
+
+//Main Entry Point
+
 app.on('ready', function(){
-   
+    //set ICON
     app.dock.setIcon(path.join(__dirname, 'icon.png'));
+    
+    //start gui
+    startGui();
+    //initialize client
+    wclient.initialize();
+});
+
+// wclient functions
+wclient.on('qr', (qr) => {
+    // Generate and scan this code with your phone
+    console.log('QR RECEIVED');
+  
+
+});
+
+wclient.on('ready', () => {
+    console.log('Client is ready!');
+    startGui();
+
+    
+});
+
+wclient.on('message', msg => {
+    if (msg.body == '!ping') {
+        msg.reply('pong');
+    }
+});
+wclient.on('disconnected', (reason) => {
+    app.quit()
+});
+
+
+
+
+// IPC HANDELERS
+ipcMain.on('data', function(event, data){
+    console.log(data);
+    mainWindow.webContents.send('respond_data', data);
+});
+
+//on opend-dialog Ipc event
+ipcMain.on('open-dialog', function(event,data){
+    dialog.showOpenDialog({properties: ['openFile'] }).then(function (response) {
+        if (!response.canceled) {
+            
+        event.sender.send('open-dialog', response.filePaths[0]);
+        
+          console.log(response.filePaths[0]);
+        } else {
+          console.log("no file selected");
+        }
+    });
+});
+
+
+
+
+
+// FUNCTIONS to START GUI
+function startGui(){
+    
     mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 700,
+        height: 550,
+        alwaysOnTop: false,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
@@ -35,39 +105,4 @@ app.on('ready', function(){
     mainWindow.on('closed', function(){
         app.quit();
     } );
-
-
-    // Now we can start the client methods
-    
-    wclient.on('qr', (qr) => {
-        // Generate and scan this code with your phone
-        console.log('QR RECEIVED');
-      //send qr to frontend via IPC
-        mainWindow.webContents.send('qr', qr);
-    
-    });
-    
-    wclient.on('ready', () => {
-        console.log('Client is ready!');
-    });
-    
-    wclient.on('message', msg => {
-        if (msg.body == '!ping') {
-            msg.reply('pong');
-        }
-    });
-    
-
-    wclient.initialize();
-});
-
-
-
-
-
-
-// IPC HANDELERS
-ipcMain.on('data', function(event, data){
-    console.log(data);
-    mainWindow.webContents.send('respond_data', data);
-});
+}
