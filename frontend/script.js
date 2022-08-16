f2fbcb94cb021262440e6493c78414109e115c93
PJKloppers <kloppers101@gmail.com>
2022-08-17 00:01:31 +0200

    ADD DATATABLES FORMATTING

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,9 @@
 const {ipcRenderer} = require('electron');
 
+// require jquery
+const $ = require('jquery');
+
+
 // get element "XLSX"
 const xlsxdiv = document.getElementById('xlsx');
 
@@ -31,25 +35,86 @@ ipcRenderer.on('open-dialog', (event, JSONDATA) => {
 // Create Ipc event for "filedata"
 ipcRenderer.on('filedata', (event, data) => {
     xlsxdiv.innerHTML = data;
+    FormatAllTables();
+    $('table').DataTable();
 } );
 
-
-
-function displaysheet(jsobj){
-    
-
-var filename = jsobj.filename;
-var sheets = jsobj.sheets;
-console.log(JSON.stringify(jsobj))
-    
-var worksheet  = XLSX.utils.aoa_to_sheet(sheets[0]);
-xlsxdiv.innerHTML = XLSX.XLSX.utils.sheet_to_html(worksheet);
-    
+//function to get all tables on the page
+function FormatAllTables(){
+    var tables = document.getElementsByTagName('table');
+    // format each table in tables
+    for(var i = 0; i < tables.length; i++){
+        formatTable(tables[i]);
+    }
 }
 
 
 
-    
+// function to add features to table element
+function formatTable(table){
+
+
+
+    // rows = table.rows;
+    // //append cell to first row
+    // var cell = rows[0].insertCell(0);
+    // //append text to cell
+    // cell.innerHTML = "Select";
+    // //set cell to first column
+
+
+
+    //loop through each row
+    // for(var i = 1; i < rows.length; i++){
+    //     //append cell to row
+    //     var cell = rows[i].insertCell(0);
+    //     //append checkbox to cell
+    //     var checkbox = document.createElement('input');
+    //     checkbox.type = "checkbox";
+    //     checkbox.name = "checkbox";
+    //     checkbox.value = "Select";
+    //     checkbox.id = "checkbox";
+    //     checkbox.checked = true;
+    //     cell.appendChild(checkbox);
+    // }
+
+
+
+    // add bootstrap class to table element
+    table.className = "table table-striped table-hover table-bordered";
+
+    // change the first row into the the table head
+    //get the first row of the table
+    var firstRow = table.rows[0];
+    // delete the first row from the table
+    table.deleteRow(0);
+
+    //create a empty table head 
+    var tableHead = document.createElement('thead');
+    //add tableHead to the table
+    table.appendChild(tableHead);
+
+    //create empty head row 
+    var headRow = document.createElement('tr');
+    //add headRow to the table head
+    tableHead.appendChild(headRow);
+
+    // change firstRow Items int ('th') items
+    for(var i = 0; i < firstRow.cells.length; i++){
+        var th = document.createElement('th');
+        th.appendChild(document.createTextNode(firstRow.cells[i].innerHTML));
+        headRow.appendChild(th);
+    }
+
+    for(var i = 0; i < table.rows.length; i++){
+        table.rows[i].className = "";
+    }
+
+}
+
+
+
+
     
     
 
